1211dacb1e115dda93c5d4bf505c4097d6d3011a
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-08 08:11:16 -0700

    gh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,6 @@ int main(void)
 	printf("size of int: %d byte(s)\n", sizeof(i));
 	printf("size of long int: %d byte(s)\n", sizeof(li));
 	printf("size of long long int: %d byte(s)\n", sizeof(lli));
-	printf("size of float: %d byte(s)\n", sizeof()f);
+	printf("size of float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
