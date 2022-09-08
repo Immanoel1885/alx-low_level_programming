c8e8f2c97be32e1652e1bc461893cb7f9148c5ac
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-08 07:28:57 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,8 @@
  * this progran prints with proper grammar, but the outcome is a piece of art,
  * Return: 0
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
