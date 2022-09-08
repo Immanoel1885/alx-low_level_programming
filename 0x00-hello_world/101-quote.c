ad56bfa07a3dbd6771bc5b7c6cfd13db425257f2
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-08 11:01:51 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
  * main - print the string in the fprintf fuction
  *
  * Description: using the main function
- * this program prints and that piece of art is useful\" - Dora Korpar, 2015-10-19
+ * this program prints and that piece of art is useful\" - Dora Korpar...
  * Return: 1
  */
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr , "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
