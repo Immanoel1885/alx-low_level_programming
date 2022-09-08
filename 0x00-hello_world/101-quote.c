801685ee2b147a6631a0015e72cddb3914c9fe00
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-08 11:47:22 -0700

    correct

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - print the string in the fprintf fuction
  *
@@ -6,8 +7,11 @@
  * this program prints and that piece of art is useful\" - Dora Korpar...
  * Return: 1
  */
+
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
