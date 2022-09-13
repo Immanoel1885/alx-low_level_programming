6c7d29468293ed43e2162e4ffc58473b9373c20b
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-13 11:29:58 -0700

    try

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function to add two numbersa & b
+ * @a: first argument of add function
+ * @b: second argument od add function
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int ttl = a + b;
+
+	_putchar(ttl);
+
+}
