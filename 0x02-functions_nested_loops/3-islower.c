73fe72b120b74c39175ad7c90fd32618f9d87115
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-13 07:33:28 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islowe - Entry point
+ * Description: checks if a given letter is lowercase
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (int islower_l(int c, locale_t locale))
+	{
+		return (1);
+	}else
+	{
+		return (0);
+
+	}
+
+}
