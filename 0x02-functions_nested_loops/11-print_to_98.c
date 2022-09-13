bb426f9cd39e1a3366e48927206828ab419fdd4f
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-13 12:07:30 -0700

    try again

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,19 +1,22 @@
 #include "main.h"
 /**
- * print_to_98.c - print the numbers between n and 98
+ * print_to_98 - print the numbers between n and 98
  * @n: the argument parsed into the above function
  * Return: 0
  */
 void print_to_98(int n)
 {
-	int i = 98 - n;
-
-	while (i < 98)
+	if (n >= 98)
 	{
-		for (; i <= 98; i++ )
-		{
-			_putchar(i);
-		}
-		i++;
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+				
 	}
 }
