6919265b82e466f3a2d644f778763b42f8090e0a
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-13 09:06:12 -0700

    second try

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,12 @@
 /**
  * _abs - finds the absolute value of a number
  * Description: this function recieves an int argument
+ * @c: the argument well be using
  * Return: 0
  */
 int _abs(int c)
 {
-	if (c > 0 || c ==0)
+	if (c > 0 || c == 0)
 	{
 		return (c);
 	}
