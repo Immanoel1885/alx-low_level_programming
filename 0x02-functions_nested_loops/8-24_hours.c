3d360fc9a0eba0d20511e6e7cbb47f635526dbf9
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-13 09:44:48 -0700

    try 1

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_buer - function prints every minute of the day
+ * Return:  0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_rem;
+	int mins_rem;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			hours_rem = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_rem + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			minutes++;
+			_putchar('\n');
+		}
+		hours++;
+		minutes = 0;
+	}
+}
