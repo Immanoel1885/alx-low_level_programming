563299a2c5442c28d00dc0457a30dda3ab952c27
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-14 08:56:55 -0700

    try 1

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * calc_multsum - entry point
+ * @n: the number to be tested
+ * @s: the sum of the numbers
+ * Return: 0
+ */
+int calc_multsum (int n, int s)
+{
+
+	while (n < 1024)
+	{
+		if ((n % 3 == 0) ||(n % 5 == 0))
+		{
+			s = s + n;
+			n++;
+		}
+		else
+			n++;
+	}
+	printf("%d\n", s);
+}
