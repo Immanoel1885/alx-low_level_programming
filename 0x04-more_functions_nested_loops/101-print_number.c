9bc7270f19a7fe0cd51a4906a32ba0972c061ae3
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-15 12:03:59 -0700

    try agin

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,5 +5,16 @@
  */
 void print_number(int n)
 {
-	_putchar(n);
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	else if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + 48);
 }
