806ad6b1fffac3edd2484bd20fd6be735b661190
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-19 09:50:07 -0700

    corrections made

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,12 +3,10 @@
  * _puts - write a new line to the stdout
  * @str: the string thaat gets a new line
  * Return: nothing
- *
  */
 void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
-		_putchar(*str); 
+		_putchar(*str);
 	_putchar('\n');
-
 }
