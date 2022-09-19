50ab29a61b2c24c8bf0448f7d34e22f68950b585
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-19 09:39:07 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - write a new line to the stdout
+ * @str: the string thaat gets a new line
+ * Return: nothing
+ *
+ */
+void _puts(char *str)
+{
+	char *p;
+
+	p = str;
+
+	_putchar(*p);
+	_putchar('\n');
+}
+
