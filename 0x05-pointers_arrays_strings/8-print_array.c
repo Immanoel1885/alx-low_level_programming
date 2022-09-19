54987c7fe1dec59dd9fe8b6b62f40f6a693943be
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-19 12:07:23 -0700

    try 3

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(*a[i]);
-		_putchar(',');
-		_putchar(' ');
+		if (i == (n - 1))
+			_putchar(a[i]);
+		else
+			_putchar(a[i]);
 	}
 	_putchar('\n');
 }
