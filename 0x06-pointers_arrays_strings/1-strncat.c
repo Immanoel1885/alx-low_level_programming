52f2732bef1c00549c16edc53bd9e11d944eb91a
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-21 05:36:32 -0700

    try 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,6 @@
  * @dest: pointer to destination input
  * @src: pointer to source input
  * @n: most number of bytes from @src
- *
  * Return: @dest
  */
 char *_strncat(char *dest, char *src, int n)
@@ -18,11 +17,12 @@ char *_strncat(char *dest, char *src, int n)
 		c++;
 
 	/**
-	 * src does ot need to be null terminated 
-	 * if it contains n or more bytes
+	 *  src does ot need to be null terminated
+	 *  if it contains n or more bytes
 	 */
 	for (i = 0; i < n && src[i] != '\0';)
 		dest[c + i] = src[i];
+
 	/*null terminate dest*/
 	dest[c + i] = '\0';
 
