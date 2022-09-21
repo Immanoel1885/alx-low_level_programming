71b97df0f6bf2c9d355ab5dfe505d3260feb81ed
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-21 05:28:17 -0700

    concat strings to n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strncat - a function that cncatenates two strings
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ *
+ * Return: @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+
+	/*find the size of dest array*/
+	while (dest[c])
+		c++;
+
+	/**
+	 * src does ot need to be null terminated 
+	 * if it contains n or more bytes
+	 */
+	for (i = 0; i < n && src[i] != '\0';)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
+
+	return (dest);
+}
