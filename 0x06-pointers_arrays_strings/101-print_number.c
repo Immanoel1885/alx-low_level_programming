2288beeaee1a0381125d0c2cf90856e7d0ce82bb
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-22 05:52:38 -0700

    try 3

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,27 +7,5 @@
  */
 void print_number(int n)
 {
-	unsigned int r, s, count;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		r = n * -1;
-	}
-	else
-		r = n;
-
-	s = r;
-	count = 1;
-
-	while (s > 9)
-	{
-		s /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((r / count) % 10) + 48);
-	}
+	_putchar(n);
 }
