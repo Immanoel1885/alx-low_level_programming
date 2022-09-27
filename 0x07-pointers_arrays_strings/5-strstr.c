c4cc1cbe12bdbdff010b97608e4b35fccd4f8ed2
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-27 06:42:43 -0700

    try again

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,27 @@
 #include "main.h"
 
 /**
- * _strstr - function that finds a needle in a haystack
- * @needle: the substring to be found
- * @haystack: the main string which  amy contain substring
+ * _strstr - function that locates a substring in a string
+ * @haystack: string
+ * @needle: substring
  *
- * Return: needle if pass, NULL if fail
+ * Return: always haystack
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (; *haystack != '\0'; haystack++)
 	{
-		int j = 0;
+		char *hay = haystack;
+		char *need = needle;
 
-		for (j = 0; needle[j] < '\0'; j++)
-			if (haystack[i + j] != needle[j])
-				break;
-		if (j == haystack[i])
-			return (needle);
-		else
-			return ('\0');
+		while (*hay == *need && *need != '\0')
+		{
+			hay++;
+			need++;
+		}
+		if (*need == '\0')
+			return (haystack);
 	}
-
-	return (needle);
-
+	return ('\0');
 }
