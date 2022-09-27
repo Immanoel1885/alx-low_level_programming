69b18bc126033bd785e2406cdc23186acfb38207
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-27 04:50:04 -0700

    my way

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - function to  get the length of a prefix substring
+ * @s: string to be compared with
+ * @accept: string to compare
+ *
+ * Return: always count
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i = 0;
+	int j = 0;
+
+	while (i != '\0')
+	{
+		while (j != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+			else if (s[i] != accept[j])
+			{
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (count);
+}
