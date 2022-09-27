4f6d00db613c710071a950125963b9664fc3b9d5
Immanoel1885 <iamikegwuatu@gmail.com>
2022-09-27 05:30:30 -0700

    try 2

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,18 +9,24 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (*s == *accept)
+		int j = 0;
+
+		while (accept[j] != '\0')
 		{
-			return (s);
+			if (s[i] == accept[j])
+			{
+				return (s + i)
+			}
+			j++;
 		}
-		s++;
+		i++;
 	}
-	if (*s != *accept)
+	if (s[i] != accept[j])
 	{
 		return ('\0');
 	}
-
-	return (s);
 }
