ae1e35e257e9cb05b955b0a181545fcb9948625c
Immanoel1885 <iamikegwuatu@gmail.com>
2022-10-05 13:21:26 -0700

    try 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * _strlen - function to return length of string
+ * @s: string
+ *
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	unsigned int i;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 /**
  * str_concat - function to concatenate string
  * @s1: the first string
@@ -11,28 +27,26 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
-	int i = 0, count = 0;
+	int i = 0, j = 0, count = 0;
 
-	while ((s1[i] + s2[1]) - 1 != '\0')
-	{
-		count++;
-		i++;
-	}
-
-	s = malloc(count * (sizeof(s1) + sizeof(s2) + 1));
-
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 
-	s1 -= '\0';
-	while (i != '\0')
+	count = (_strlen(s1) + _strlen(s2) + 1);
+
+	s = malloc(count * sizeof(char));
+
+	if (s == 0)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
 	{
-		s[i] = s1[i] + s2[i];
+		s[i] = s2[j];
 		i++;
 	}
-	s[i] = '\0';
 	return (s);
 }
