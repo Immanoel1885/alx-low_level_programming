f3eaf426aa5fc99eec6b0ce704bd2382704fa756
Immanoel1885 <iamikegwuatu@gmail.com>
2022-10-14 06:44:58 -0700

    write opcodes for main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: program that prints the opcodes of ts own main function
+ * @argc: argument count
+ * @argv: argument variables
+ *
+ * Return: success
+ */
+int main(int argc, cahr *argv[])
+{
+	int number_of_bytes, i;
+	char *opcode = (char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	number_of_bytes = atoi(argv[1]);
+
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	i = 0;
+	while (i < number_of_bytes)
+	{
+		printf("%02x", opcode[i] & 0xFF);
+		if (i != number_of_bytes - 1)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
