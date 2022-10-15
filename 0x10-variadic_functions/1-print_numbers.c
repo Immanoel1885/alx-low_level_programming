5f0b41cf3ed33e77f7e430a13d22a88eae60f9a5
Immanoel1885 <iamikegwuatu@gmail.com>
2022-10-15 06:33:42 -0700

    correct mistakes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,9 +16,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(init, unsigned int));
-		if (sparator != NULL) && i != (n - 1)
+		if (separator != NULL && i != (n - 1));
 			printf("%s", separator);
 	}
-	printf('\n');
+	printf("\n");
 	va_end(init);
 }
