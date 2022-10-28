19d121d15511cd8fdd73e3c88481a23a698ca2f4
Immanoel1885 <iamikegwuatu@gmail.com>
2022-10-28 09:15:12 -0700

    correct betty error

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,7 +14,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (i = 63; i >= 0; i--)
 	{
-		if (((n ^ m ) >> i) & 1)
+		if (((n ^ m) >> i) & 1)
 			num++;
 	}
 	return (num);
